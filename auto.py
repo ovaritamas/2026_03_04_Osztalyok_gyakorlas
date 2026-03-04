aa417bf75e17a0d4ce35d8ae7085e4b04ba47830
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2026-03-04 09:14:54 +0100

    2.ora

--- auto.py
+++ auto.py
@@ -3,7 +3,18 @@ class Auto:
         self.marka = marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev_
+        self.sebesseg = 0
 
     def __str__(self):
-        return f"{self.marka} {self.tipus} ({self.gyartasi_ev})"
+        return f"{self.marka} {self.tipus} ({self.gyartasi_ev}), sebesség:{self.sebesseg}km/h"
+    
+    def gyorsit(self, ertek):
+        self.sebesseg += ertek
+        if self.sebesseg > 200:
+            self.sebesseg = 200
+
+    def fekez(self, ertek):
+        self.sebesseg -= ertek
+        if self.sebesseg < 0:
+            self.sebesseg = 0
         
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from auto import Auto
+import random as r
 
 auto1 = Auto("Toyota", "Corolla", 2006)
 auto2 = Auto("Nissan", "Note", 2015)
+auto3 = Auto("Ford", "Focus", 2005)
+auto4 = Auto("Volkswagen", "Passat", 2004)
+auto5 = Auto("Opel", "Astra", 2007)
 
-print(auto1)
-print(auto2)
+autok = [auto1, auto2, auto3, auto4, auto5]
+for auto in autok:
+    print(auto)
+
+for auto in autok:
+    auto.gyorsit(r.randint(10, 170))  
+    print(auto)
+
+#print(auto1)
+#print(auto2)
+
+#auto1.gyorsit(150)
+#print(auto1)
