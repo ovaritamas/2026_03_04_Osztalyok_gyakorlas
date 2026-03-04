008216457ef1857252cb92267e68142e7fd397a1
ovaritamas <ovari.tamas@diak.szbi-pg.hu>
2026-03-04 10:12:37 +0100

    3.ora

--- auto.py
+++ auto.py
@@ -1,9 +1,11 @@
 class Auto:
-    def __init__(self, marka_, tipus_, gyartasi_ev_):
+    def __init__(self, marka_, tipus_, gyartasi_ev_, uzemanyag_, fogyasztas_):
         self.marka = marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev_
         self.sebesseg = 0
+        self.uzemanyag = 50
+        self.uzemanyag = uzemanyag_
 
     def __str__(self):
         return f"{self.marka} {self.tipus} ({self.gyartasi_ev}), sebesség:{self.sebesseg}km/h"
@@ -17,4 +19,8 @@ class Auto:
         self.sebesseg -= ertek
         if self.sebesseg < 0:
             self.sebesseg = 0
-        
+
+    def tankol(self, mennyiseg):
+        self.uzemanyag += mennyiseg
+        if self.uzemanyag > 50:
+            self.uzemanyag = 50    
